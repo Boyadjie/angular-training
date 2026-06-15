196e6e4d24fa1f10e27c0caea5f4ca3f8acd31e9
Emeric Boyadjian <emeric@Mac.lan>
2026-06-15 15:43:29 +0200

    ex 5 : fixed tests

--- Exercises/workspaces/src/app/app.spec.ts
+++ Exercises/workspaces/src/app/app.spec.ts
@@ -1,23 +1,44 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { By } from '@angular/platform-browser';
+import { ProductCard } from './product-card/product-card';
 
 describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to my first component');
+  it('It should display the products', () => {
+    const productElements = fixture.debugElement.queryAll(By.directive(ProductCard));
+    expect(productElements.length).toBe(4);
+
+    productElements.forEach((productElement, index) => {
+      const productComponent: ProductCard = productElement.componentInstance;
+      expect(productComponent.product()).toBe(component.products[index]);
+    });
+  });
+
+  it('should update the total when "addToBasket" class method is called', () => {
+    // Given
+    component.total = 99;
+
+    // When
+    component.addToBasket(component.products[1]);
+
+    // Then
+    expect(component.total).toBe(99 + component.products[1].price);
   });
 });
--- Exercises/workspaces/src/app/product-card/product-card.spec.ts
+++ Exercises/workspaces/src/app/product-card/product-card.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { vi } from 'vitest';
 
 import { ProductCard } from './product-card';
 
@@ -13,10 +14,45 @@ describe('ProductCard', () => {
 
     fixture = TestBed.createComponent(ProductCard);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('product', {
+      id: 'test-id',
+      title: 'test title',
+      description: 'test description',
+      photo: '/assets/test-picture.jpg',
+      price: 20,
+      stock: 2,
+    });
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the product title', () => {
+    const titleElement = fixture.nativeElement.querySelector('.card-title');
+    expect(titleElement.textContent).toContain('test title');
+  });
+
+  it('should display the product description', () => {
+    const descriptionElement = fixture.nativeElement.querySelector('.card-header');
+    expect(descriptionElement.textContent).toContain('test description');
+  });
+  
+  it('should display the product price', () => {
+    const priceElement = fixture.nativeElement.querySelector('.card-text');
+    expect(priceElement.textContent).toContain('20 €');
+  });
+
+  it('should display the product photo as image url', () => {
+    const imageElement = fixture.nativeElement.querySelector('.card-img-top');
+    expect(imageElement.src).toContain('/assets/test-picture.jpg');
+  });
+
+  it('should emit addToBasket event with the given product when the button is clicked', () => {
+    vi.spyOn(component.addToBasket, 'emit');
+    const buttonElement = fixture.nativeElement.querySelector('button');
+    buttonElement.click();
+    expect(component.addToBasket.emit).toHaveBeenCalledWith(component.product());
   });
 });
