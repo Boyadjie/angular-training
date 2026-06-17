973d277075c9026c7d9e29c5dfe7eceaa47bc6f6
Emeric Boyadjian <emeric@Mac.lan>
2026-06-17 15:53:39 +0200

    feat(menu): add number of item count displayed in the menu

--- Exercises/workspaces/src/app/menu/menu.ts
+++ Exercises/workspaces/src/app/menu/menu.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Basket } from '../basket/basket';
 
 @Component({
   selector: 'app-menu',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {}
+export class Menu {
+  basketService = inject(Basket)
+
+  numberOfItems = computed<number>(() => this.basketService.items().length)
+}
